44f9af09d30675873ae8ab60c817d8ee98e5f146
mKO9876 <martakokic98@gmail.com>
2024-12-14 00:40:15 +0100

    Fixed queries

--- queries.sql
+++ queries.sql
@@ -21,7 +21,7 @@ T.TrainerLastName || ' ' || SUBSTRING(T.TrainerName, 1, 1) || '.' AS Trainer,
 tw.Time
 FROM Workouts w
 JOIN TrainerWorkout tw ON tw.WorkoutId = w.WorkoutId
-JOIN Trainers t ON tw.TrainerId = t.TrainerId
+JOIN Trainers t ON tw.TrainerId = t.TrainerId;
 
 
 --3 -> Top 3 fitness centra s najvećim brojem aktivnosti u rasporedu
@@ -38,22 +38,21 @@ LIMIT 3;
 
 --4 -> Po svakom terneru koliko trenutno aktivnosti vodi; 
 --ako nema aktivnosti, ispiši “DOSTUPAN”, ako ima do 3 ispiši “AKTIVAN”, a ako je na više ispiši “POTPUNO ZAUZET”.
-SELECT t.TrainerName,
-t.TrainerLastName,
-COUNT(tw.TrainerId) AS Sum_Activity,
+SELECT t.TrainerName, t.TrainerLastName, COUNT(tw.TrainerId),
 CASE
 	WHEN COUNT(tw.TrainerId) = 0 THEN 'DOSTUPAN'
-	WHEN COUNT(tw.TrainerId) <= 3 THEN 'AKTIVAN'
-	ELSE 'POTPUNO ZAUZET'
-	END AS Status
+    WHEN COUNT(tw.TrainerId) <= 3 THEN 'AKTIVAN'
+    ELSE 'POTPUNO ZAUZET'
+    END AS Status
 FROM Trainers t
-JOIN TrainerWorkout tw ON tw.WorkoutId = T.TrainerId
+JOIN TrainerWorkout tw ON tw.TrainerId = t.TrainerId
+GROUP BY t.TrainerName, t.TrainerLastName;
 
 
 --5 -> Imena svih članova koji trenutno sudjeluju na nekoj aktivnosti.
 SELECT u.UserName, u.UserLastName
 FROM Users u
-JOIN UerActivity ua ON ua.UserId = u.UserId
+JOIN UserActivity ua ON ua.UserId = u.UserId
 JOIN TrainerWorkout tw ON tw.WorkoutId = ua.WorkoutId AND tw.TrainerId = ua.TrainerId
 WHERE DATE(tw.Time) = CURRENT_DATE;
 
@@ -61,7 +60,7 @@ WHERE DATE(tw.Time) = CURRENT_DATE;
 --6 -> Sve trenere koji su vodili barem jednu aktivnost između 2019. i 2022.
 SELECT t.TrainerName, t.TrainerLastName FROM Trainers t
 JOIN TrainerWorkout tw ON tw.TrainerId = t.TrainerId
-EXTRACT(YEAR FROM tw.Time) BETWEEN 2019 AND 2022;
+WHERE EXTRACT(YEAR FROM tw.Time) BETWEEN 2019 AND 2022;
 
 
 --7 -> Prosječan broj sudjelovanja po tipu aktivnosti po svakoj državi.
@@ -82,7 +81,7 @@ SELECT c.CountryName, COUNT(ua.UserId) AS TotalParticipants
 FROM Countries c
 JOIN Trainers t ON t.CountryId = c.CountryId
 JOIN TrainerWorkout tw ON tw.TrainerId = t.TrainerId
-JOIN Workout ON tw.WorkoutId = w.WorkoutId
+JOIN Workouts w ON tw.WorkoutId = w.WorkoutId
 JOIN UserActivity ua ON ua.WorkoutId = tw.WorkoutId
 WHERE w.WorkoutType = 'Injury rehabilitation'
 GROUP BY c.CountryName
@@ -93,25 +92,26 @@ LIMIT 10;
 --9 -> Ako aktivnost nije popunjena, ispiši uz nju “IMA MJESTA”, a ako je popunjena ispiši “POPUNJENO”
 SELECT w.WorkoutName,
 CASE 
-	 WHEN COUNT(*) >= tw.Capacity THEN 'POPUNJENO'
-	 ELSE 'IMA MJESTA'
-	 END
+	WHEN COUNT(*) >= tw.Capacity THEN 'POPUNJENO'
+	ELSE 'IMA MJESTA'
+    END AS Status
 FROM TrainerWorkout tw
 JOIN UserActivity ua ON ua.WorkoutId = tw.WorkoutId AND ua.TrainerId = tw.TrainerId
 JOIN Workouts w ON w.WorkoutId = tw.WorkoutId
-GROUP BY (tw.TrainerId, tw.WorkoutId);
+GROUP BY tw.TrainerId, tw.WorkoutId, w.WorkoutName;
 
 
 --10 -> 10 najplaćenijih trenera, ako po svakoj aktivnosti dobije prihod kao brojSudionika * cijenaPoTerminu
-SELECT t.TrainerName, t.TrainerLastName,SUM(ua.Participants * w.PricePerSession) AS TotalEarnings 
+SELECT t.TrainerName, t.TrainerLastName, SUM(ua.Participants * tw.Price) AS TotalEarnings
 FROM Trainers t
 JOIN TrainerWorkout tw ON tw.TrainerId = t.TrainerId
 JOIN (
-	SELECT WorkoutId, TrainerId, COUNT(UserId) AS Participants
+    SELECT WorkoutId, TrainerId, COUNT(UserId) AS Participants
     FROM UserActivity
     GROUP BY WorkoutId, TrainerId
-)
-GROUP BY t.TrainerId
+) ua ON ua.WorkoutId = tw.WorkoutId AND ua.TrainerId = tw.TrainerId
+GROUP BY t.TrainerId, t.TrainerName, t.TrainerLastName
 ORDER BY TotalEarnings DESC
 LIMIT 10;
+
 
